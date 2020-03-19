c1986d8cff0a0c839cf3730456cd7e025407679c
Paul FLeming <pfleming@heroku.com>
2020-03-19 21:29:14 +0000

    Moved WhiteNoise requirements to MIDDLEWARE in settings.py

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -10,8 +10,8 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
+#from whitenoise.djano import DjangoWhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
-application = DjangoWhiteNoise(application)
+application = get_wsgi_application()
