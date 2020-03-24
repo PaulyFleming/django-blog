766b45d2613a5415ea4f3b2c315a9303c85bf54a
Paul FLeming <pfleming@heroku.com>
2020-03-24 14:03:43 +0000

    Updated ALOWED_HOSTS in settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '%-!4_c+44_^+dn-zn%5d8q_!=+3pn64e1#*5@t2a%1kcm=8e5)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
